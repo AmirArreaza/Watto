bdff5171816ab9a772afeb4aaa5f5f8801f9ece2
AmirArreaza <ajaarreazav@gmail.com>
2012-09-25 09:03:44 -0430

    Agregado blacklists al locale

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
 	factory :user do
-		sequence(:id_number)		{ |n| "#{n}#{n}#{n}#{n}#{n}#{n}#{n}#{n}"}
+		sequence(:id_number)	{ |n| "#{n}#{n}#{n}#{n}#{n}#{n}#{n}#{n}"}
 		sequence(:first_name)  	{ |n| "Nombre #{n}"}
     	sequence(:last_name) 	{ |n| "Apellido #{n}"}
     	sequence(:email)		{ |n| "Email_#{}{n}@email.com"}
