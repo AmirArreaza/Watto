3ef137888efe7a17e36f2b22495b93be31a3aa5c
AmirArreaza <ajaarreazav@gmail.com>
2012-10-10 15:13:59 -0430

    Product con reservations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,6 @@
 class Product < ActiveRecord::Base
-  attr_accessible :description, :name, :stock
+	attr_accessible :description, :name, :stock
+
+	has_many :reservations
+  	has_many :users, :through => :reservations, :dependent => :destroy
 end
