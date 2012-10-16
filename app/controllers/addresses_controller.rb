b4ad5c84f70867eea959730fac11c7f8460471e4
AmirArreaza <ajaarreazav@gmail.com>
2012-10-16 17:20:35 -0430

    Avances en select anidados

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,15 +1,14 @@
 class AddressesController < ApplicationController
-
 	def show
-
 	end
 
 	def new
 		@address = Address.new
+		@states  = Address.find(:all, :order=>"address_name", :conditions => {:location_type => "State"})
+		@cities  = []
 	end
 
 	def create
-
 		if params[:address][:belong_address].to_i == 0
 			params[:address][:belong_address] = nil
 		else
@@ -40,5 +39,12 @@ class AddressesController < ApplicationController
 		Address.find(params[:id]).destroy
     	redirect_to 'new'
 	end
+
+	def update_cities
+    	# updates artists and songs based on genre selected
+    	@states = Address.find(:all, :order=>"address_name", :conditions => {:location_type => "State"})
+    	@cities = Address.find(:all, :order=>"address_name", :conditions => {:location_type => "City", :belong_address => params[:genre_id]})
+    	@cities = @cities.map{|a| [a.address_name, a.id]}.insert(0, "Select an City")
+  	end	
 
 end
