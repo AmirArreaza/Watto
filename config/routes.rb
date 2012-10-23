cc05e175d037bcce13b75842f6e22c58dd2f2845
AmirArreaza <ajaarreazav@gmail.com>
2012-10-23 18:14:31 -0430

    Agregada barra de navegacion

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,13 @@ Watto::Application.routes.draw do
   root to: 'static_pages#home'
 
   resources :sessions, 	only: [:new, :create, :destroy]
-  resources :users, only: [:new, :create, :show]
+  resources :users, 	only: [:new, :create, :show]
   resources :addresses, only: [:new, :create, :show, :edit, :update]
   	match "addresses/update_cities/:id" => "addresses#update_cities"
   	match "addresses/update_states/:id" => "addresses#update_states"
-
-
   resources :companies, only: [:show, :new, :create, :edit, :update, :destroy]
+
+  root to: 'users#new'
 
   match '/signup',  to: 'users#new'
   match '/signin',  to: 'sessions#new'
